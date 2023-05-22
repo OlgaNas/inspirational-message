0d9417da8f67cf334e7e8bbf649db9fdfaea6063
Olga Nastaja <olga.vlasova321@gmail.com>
2023-05-22 18:35:57 +0100

    Add const

--- inspirational-messages.js
+++ inspirational-messages.js
@@ -1,2 +1,10 @@
 //Random inspirational-messages
-//
+const youAre = [];
+const when = ['Today', 'Soon', 'Tomorrow', 'This year', 'This month'];
+const what = [];
+const how = [];
+
+
+
+
+console.log(`You are ${youAre}. ${when} your ${what} will be ${how}!`);
