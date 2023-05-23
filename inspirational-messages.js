54678d2ad18d87cd818a05499d8af80b0ac67783
Olga Nastaja <olga.vlasova321@gmail.com>
2023-05-23 17:48:05 +0100

    Add 3 arrays

--- inspirational-messages.js
+++ inspirational-messages.js
@@ -1,10 +1,9 @@
 //Random inspirational-messages
-const youAre = [];
+const you = ['have the best laugh', 'light up the room', 'have a great sense of humor', 'like sunshine on a rainy day', ' bring out the best in other people', 'are strong', 'are making a difference'];
 const when = ['Today', 'Soon', 'Tomorrow', 'This year', 'This month'];
-const what = [];
-const how = [];
+const what = ['everything will be ok', 'happiness will come to you', 'you will meet someone special', 'you will save lots of money', 'good news will come to you', 'you will have good time with your family', 'weather will be good'];
 
 
+function
 
-
-console.log(`You are ${youAre}. ${when} your ${what} will be ${how}!`);
+    console.log(`You ${youAre}. ${when} ${what}!`);
